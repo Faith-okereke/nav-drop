fce5d404799c92c493d14f820c2dc257322eb6de
FAITH <faithokereke26@gmail.com>
2023-03-17 17:50:35 +0100

    commit change

---
+++ script.js
@@ -0,0 +1,34 @@
+function openFirstOne(){
+    const feature = document.getElementById("feature");
+    const content = document.getElementById("content");
+    const image = document.getElementById('image')
+    if (content.style.display === "none" && image.src.match("images/icon-arrow-down.svg")) {
+        content.style.display = "block";
+        image.setAttribute("src", "images/icon-arrow-up.svg");
+      } else {
+        content.style.display = "none";
+        image.setAttribute("src", "images/icon-arrow-down.svg");
+      }
+}
+function openSecondOne(){
+    const content2 = document.getElementById('content-2');
+    const image2 = document.getElementById('image-2');
+    if (content2.style.display === "none" && image2.src.match("images/icon-arrow-down.svg")) {
+        content2.style.display = "block";
+        image2.setAttribute("src", "images/icon-arrow-up.svg");
+      } else {
+        content2.style.display = "none";
+        image2.setAttribute("src", "images/icon-arrow-down.svg");
+      }
+}
+ 
+const openNav = () =>{
+  const overall = document.getElementById('all');
+  overall.style.width="200px"
+
+}
+const closeNav = () =>{
+  const overall = document.getElementById('all');
+  overall.style.width = "0%"
+}
+  
